3ec8bd7003c2b6622ec18d020e92cccf6db14d26
Whs9801 <williamhilmys@gmail.com>
2025-04-20 23:15:23 +0700

    edit contact information

--- company-profile/src/components/Contact.js
+++ company-profile/src/components/Contact.js
@@ -3,9 +3,9 @@ import React from "react";
 const Contact = () => (
   <section id="contact" className="contact dark-section">
       <h2>Kontak Kami</h2>
-      <p>Email: info@sandikalam.co.id</p>
-      <p>WhatsApp: +62 812-3456-7890</p>
-      <p>Alamat: SPPG Dawuan, Subang</p>
+      <p>Email: sandikalamidn@gmail.com</p>
+      <p>WhatsApp: +62 821-2071-7659</p>
+      <p>Alamat: Jl. Balai Desa, Gang Ngragan I No. 5, Desa/Kelurahan Gulang, Kec. Mejobo, Kab. Kudus, Provinsi Jawa Tengah</p>
     </section>
 );
 
